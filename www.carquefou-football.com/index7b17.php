caeb2aec1e468843cc7a48396333810d24cf0956
Jean-Philippe Le Goff <jean-philippelegoff@Jean-Philippes-MacBook-Pro.local>
2017-01-31 17:20:51 +0100

    init website

---
+++ www.carquefou-football.com/index7b17.php
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="fr-fr">
+	<title type="text">Accueil</title>
+	<subtitle type="text">L'USJA Carquefou Football, toutes les équipes de foot, les résultats, les actualités.</subtitle>
+	<link rel="alternate" type="text/html" href="http://www.carquefou-football.com"/>
+	<id>http://www.carquefou-football.com/index.php</id>
+	<updated>2017-01-31T16:12:27Z</updated>
+	<generator uri="http://joomla.org" version="1.5"></generator>
+<link rel="self" type="application/atom+xml" href="http://www.carquefou-football.com/index.php?format=feed&amp;type=atom" />
+	<entry>
+		<title>RESULTATS-AGENDA</title>
+		<link rel="alternate" type="text/html" href="http://www.carquefou-football.com/component/content/article/94-banniere/1236-resultats-agenda.html"/>
+		<published>2015-04-06T17:29:11Z</published>
+		<updated>2015-04-06T17:29:11Z</updated>
+		<id>http://www.carquefou-football.com/component/content/article/94-banniere/1236-resultats-agenda.html</id>
+		<author>
+			<name>Vincent</name>
+		<email>vince.charlot@free.fr</email>
+		</author>
+		<summary type="html">&lt;p&gt;&lt;a href=&quot;http://atlantique.fff.fr/competitions/php/club/club_agenda.php?cl_no=1337&quot; target=&quot;_blank&quot;&gt;&lt;img style=&quot;font-size: 1.1em;&quot; src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/AGENDA4.jpg&quot; alt=&quot;AGENDA4&quot; width=&quot;290&quot; height=&quot;84&quot; /&gt;&lt;/a&gt;&lt;span style=&quot;font-size: 1.1em;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;a href=&quot;http://atlantique.fff.fr/competitions/php/club/club_resultat.php?cl_no=1337&quot; target=&quot;_blank&quot;&gt;&lt;img style=&quot;font-size: 1.1em;&quot; src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/RESULTATS4.jpg&quot; alt=&quot;RESULTATS4&quot; width=&quot;290&quot; height=&quot;84&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p&gt;&lt;a href=&quot;http://atlantique.fff.fr/competitions/php/club/club_agenda.php?cl_no=1337&quot; target=&quot;_blank&quot;&gt;&lt;img style=&quot;font-size: 1.1em;&quot; src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/AGENDA4.jpg&quot; alt=&quot;AGENDA4&quot; width=&quot;290&quot; height=&quot;84&quot; /&gt;&lt;/a&gt;&lt;span style=&quot;font-size: 1.1em;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;a href=&quot;http://atlantique.fff.fr/competitions/php/club/club_resultat.php?cl_no=1337&quot; target=&quot;_blank&quot;&gt;&lt;img style=&quot;font-size: 1.1em;&quot; src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/RESULTATS4.jpg&quot; alt=&quot;RESULTATS4&quot; width=&quot;290&quot; height=&quot;84&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</content>
+	</entry>
+	<entry>
+		<title>PROMO BOUTIQUE</title>
+		<link rel="alternate" type="text/html" href="http://www.carquefou-football.com/component/content/article/94-banniere/1264-promo-boutique.html"/>
+		<published>2015-10-13T08:27:17Z</published>
+		<updated>2015-10-13T08:27:17Z</updated>
+		<id>http://www.carquefou-football.com/component/content/article/94-banniere/1264-promo-boutique.html</id>
+		<author>
+			<name>Alexandra (Images Associees)</name>
+		<email>alexandra@images-associees.com</email>
+		</author>
+		<summary type="html">&lt;p&gt;&lt;a href=&quot;http://www.carquefou-football.com/la-boutique.html&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/banners/boutique.png&quot; alt=&quot;boutique&quot; width=&quot;553&quot; height=&quot;118&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p&gt;&lt;a href=&quot;http://www.carquefou-football.com/la-boutique.html&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/banners/boutique.png&quot; alt=&quot;boutique&quot; width=&quot;553&quot; height=&quot;118&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</content>
+	</entry>
+	<entry>
+		<title>Voeux 2017</title>
+		<link rel="alternate" type="text/html" href="http://www.carquefou-football.com/le-club/actualites/1379-voeux2017.html"/>
+		<published>2016-12-31T14:49:49Z</published>
+		<updated>2016-12-31T14:49:49Z</updated>
+		<id>http://www.carquefou-football.com/le-club/actualites/1379-voeux2017.html</id>
+		<author>
+			<name>Jean-Philippe Le Goff</name>
+		<email>jphlegoff@gmail.com</email>
+		</author>
+		<summary type="html">&lt;p&gt;&lt;a href=&quot;http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png&quot; target=&quot;_blank&quot; class=&quot;jcepopup&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png&quot; alt=&quot;carte de voeux 2017 v2 site&quot; width=&quot;600&quot; height=&quot;502&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p&gt;&lt;a href=&quot;http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png&quot; target=&quot;_blank&quot; class=&quot;jcepopup&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png&quot; alt=&quot;carte de voeux 2017 v2 site&quot; width=&quot;600&quot; height=&quot;502&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</content>
+	</entry>
+	<entry>
+		<title>Bureau 2016 2017</title>
+		<link rel="alternate" type="text/html" href="http://www.carquefou-football.com/le-club/actualites/1377-bureau20162017.html"/>
+		<published>2016-12-14T09:48:54Z</published>
+		<updated>2016-12-14T09:48:54Z</updated>
+		<id>http://www.carquefou-football.com/le-club/actualites/1377-bureau20162017.html</id>
+		<author>
+			<name>Rozenn Fady</name>
+		<email>rozenn.fady@outlook.com</email>
+		</author>
+		<summary type="html">&lt;p&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Hier soir, le conseil d'administration a élu son nouveau bureau &lt;/span&gt;&lt;/p&gt;
+&lt;ul&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Président : Anthony RETIÈRE&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Vice-Président : Laurent BENEC'H&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Trésorier : Henri FROMENTIN&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Trésorier adjoint : Guy LE MELLINER&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Secrétaire : Gérard ESTÈVE&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Secrétaire adjointe : Rozenn FADY&lt;br /&gt;&lt;/span&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;&lt;a href=&quot;http://www.carquefou-football.com/index.php?option=com_jce&amp;amp;view=popup&amp;amp;tmpl=component&quot; onclick=&quot;window.open(this.href+'&amp;amp;img=images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png&amp;amp;title=','','scrollbars=yes,resizable=yes,location=yes,menubar=yes,status=yes,toolbar=yes,left=0,top=0');return false;&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png&quot; alt=&quot;Présidents&quot; width=&quot;361&quot; height=&quot;244&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/a&gt;&lt;/span&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Hier soir, le conseil d'administration a élu son nouveau bureau &lt;/span&gt;&lt;/p&gt;
+&lt;ul&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Président : Anthony RETIÈRE&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Vice-Président : Laurent BENEC'H&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Trésorier : Henri FROMENTIN&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Trésorier adjoint : Guy LE MELLINER&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Secrétaire : Gérard ESTÈVE&lt;/span&gt;&lt;/li&gt;
+&lt;li&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;Secrétaire adjointe : Rozenn FADY&lt;br /&gt;&lt;/span&gt;&lt;/li&gt;
+&lt;/ul&gt;
+&lt;p&gt;&lt;span style=&quot;font-family: verdana,geneva;&quot;&gt;&lt;a href=&quot;http://www.carquefou-football.com/index.php?option=com_jce&amp;amp;view=popup&amp;amp;tmpl=component&quot; onclick=&quot;window.open(this.href+'&amp;amp;img=images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png&amp;amp;title=','','scrollbars=yes,resizable=yes,location=yes,menubar=yes,status=yes,toolbar=yes,left=0,top=0');return false;&quot;&gt;&lt;img src=&quot;http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png&quot; alt=&quot;Présidents&quot; width=&quot;361&quot; height=&quot;244&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/a&gt;&lt;/span&gt;&lt;/p&gt;</content>
+	</entry>
+	<entry>
+		<title>Retour sur l'assemblée générale</title>
+		<link rel="alternate" type="text/html" href="http://www.carquefou-football.com/le-club/actualites/1375-ag2016retour.html"/>
+		<published>2016-12-03T13:09:56Z</published>
+		<updated>2016-12-03T13:09:56Z</updated>
+		<id>http://www.carquefou-football.com/le-club/actualites/1375-ag2016retour.html</id>
+		<author>
+			<name>Rozenn Fady</name>
+		<email>rozenn.fady@outlook.com</email>
+		</author>
+		<summary type="html">&lt;p&gt;Un grand merci à tous les adhérents qui se sont déplacés ou ont donné pouvoir.&lt;/p&gt;
+&lt;p&gt;Lors de cette AG, Gilles Fourage co-président en fin de mandat, a annoncé qu'il ne renouvellait pas son mandat, pour des raisons d'éloignement géographique futur.&lt;/p&gt;
+&lt;p&gt;Nous avons procédé à l'élection ou au renouvellement de 8 administrateurs.&lt;/p&gt;
+&lt;p&gt;Le prochain conseil d'administration&amp;nbsp; élira le bureau de l'USJA pour la saison 2016-2017.&lt;/p&gt;
+&lt;p&gt;Retour en images :&lt;/p&gt;
+</summary>
+		<content type="html">&lt;p&gt;Un grand merci à tous les adhérents qui se sont déplacés ou ont donné pouvoir.&lt;/p&gt;
+&lt;p&gt;Lors de cette AG, Gilles Fourage co-président en fin de mandat, a annoncé qu'il ne renouvellait pas son mandat, pour des raisons d'éloignement géographique futur.&lt;/p&gt;
+&lt;p&gt;Nous avons procédé à l'élection ou au renouvellement de 8 administrateurs.&lt;/p&gt;
+&lt;p&gt;Le prochain conseil d'administration&amp;nbsp; élira le bureau de l'USJA pour la saison 2016-2017.&lt;/p&gt;
+&lt;p&gt;Retour en images :&lt;/p&gt;
+</content>
+	</entry>
+</feed>
---
+++ www.carquefou-football.com/indexc0d0.php
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Accueil</title>
+		<description>L'USJA Carquefou Football, toutes les équipes de foot, les résultats, les actualités.</description>
+		<link>http://www.carquefou-football.com/component/content/frontpage.html</link>
+		<lastBuildDate>Tue, 31 Jan 2017 16:12:26 +0000</lastBuildDate>
+		<generator></generator>
+		<language>fr-fr</language>
+		<item>
+			<title>RESULTATS-AGENDA</title>
+			<link>http://www.carquefou-football.com/component/content/article/94-banniere/1236-resultats-agenda.html</link>
+			<guid>http://www.carquefou-football.com/component/content/article/94-banniere/1236-resultats-agenda.html</guid>
+			<description><![CDATA[<p><a href="http://atlantique.fff.fr/competitions/php/club/club_agenda.php?cl_no=1337" target="_blank"><img style="font-size: 1.1em;" src="http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/AGENDA4.jpg" alt="AGENDA4" width="290" height="84" /></a><span style="font-size: 1.1em;">&nbsp;</span><a href="http://atlantique.fff.fr/competitions/php/club/club_resultat.php?cl_no=1337" target="_blank"><img style="font-size: 1.1em;" src="http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2014-2015/RESULTATS4.jpg" alt="RESULTATS4" width="290" height="84" /></a></p>]]></description>
+			<author>vince.charlot@free.fr (Vincent)</author>
+			<category>frontpage</category>
+			<pubDate>Mon, 06 Apr 2015 17:29:11 +0000</pubDate>
+		</item>
+		<item>
+			<title>PROMO BOUTIQUE</title>
+			<link>http://www.carquefou-football.com/component/content/article/94-banniere/1264-promo-boutique.html</link>
+			<guid>http://www.carquefou-football.com/component/content/article/94-banniere/1264-promo-boutique.html</guid>
+			<description><![CDATA[<p><a href="http://www.carquefou-football.com/la-boutique.html"><img src="http://www.carquefou-football.com/images/banners/boutique.png" alt="boutique" width="553" height="118" /></a></p>]]></description>
+			<author>alexandra@images-associees.com (Alexandra (Images Associees))</author>
+			<category>frontpage</category>
+			<pubDate>Tue, 13 Oct 2015 08:27:17 +0000</pubDate>
+		</item>
+		<item>
+			<title>Voeux 2017</title>
+			<link>http://www.carquefou-football.com/le-club/actualites/1379-voeux2017.html</link>
+			<guid>http://www.carquefou-football.com/le-club/actualites/1379-voeux2017.html</guid>
+			<description><![CDATA[<p><a href="http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png" target="_blank" class="jcepopup"><img src="http://www.carquefou-football.com/images/carte_de_voeux_2017_v2_site.png" alt="carte de voeux 2017 v2 site" width="600" height="502" style="display: block; margin-left: auto; margin-right: auto;" /></a></p>]]></description>
+			<author>jphlegoff@gmail.com (Jean-Philippe Le Goff)</author>
+			<category>frontpage</category>
+			<pubDate>Sat, 31 Dec 2016 14:49:49 +0000</pubDate>
+		</item>
+		<item>
+			<title>Bureau 2016 2017</title>
+			<link>http://www.carquefou-football.com/le-club/actualites/1377-bureau20162017.html</link>
+			<guid>http://www.carquefou-football.com/le-club/actualites/1377-bureau20162017.html</guid>
+			<description><![CDATA[<p><span style="font-family: verdana,geneva;">Hier soir, le conseil d'administration a élu son nouveau bureau </span></p>
+<ul>
+<li><span style="font-family: verdana,geneva;">Président : Anthony RETIÈRE</span></li>
+<li><span style="font-family: verdana,geneva;">Vice-Président : Laurent BENEC'H</span></li>
+<li><span style="font-family: verdana,geneva;">Trésorier : Henri FROMENTIN</span></li>
+<li><span style="font-family: verdana,geneva;">Trésorier adjoint : Guy LE MELLINER</span></li>
+<li><span style="font-family: verdana,geneva;">Secrétaire : Gérard ESTÈVE</span></li>
+<li><span style="font-family: verdana,geneva;">Secrétaire adjointe : Rozenn FADY<br /></span></li>
+</ul>
+<p><span style="font-family: verdana,geneva;"><a href="http://www.carquefou-football.com/index.php?option=com_jce&amp;view=popup&amp;tmpl=component" onclick="window.open(this.href+'&amp;img=images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png&amp;title=','','scrollbars=yes,resizable=yes,location=yes,menubar=yes,status=yes,toolbar=yes,left=0,top=0');return false;"><img src="http://www.carquefou-football.com/images/stories/DECO-ARTICLE/Saison_2015-2016/thumbnails/thumbnails/Présidents.png" alt="Présidents" width="361" height="244" style="display: block; margin-left: auto; margin-right: auto;" /></a></span></p>]]></description>
+			<author>rozenn.fady@outlook.com (Rozenn Fady)</author>
+			<category>frontpage</category>
+			<pubDate>Wed, 14 Dec 2016 09:48:54 +0000</pubDate>
+		</item>
+		<item>
+			<title>Retour sur l'assemblée générale</title>
+			<link>http://www.carquefou-football.com/le-club/actualites/1375-ag2016retour.html</link>
+			<guid>http://www.carquefou-football.com/le-club/actualites/1375-ag2016retour.html</guid>
+			<description><![CDATA[<p>Un grand merci à tous les adhérents qui se sont déplacés ou ont donné pouvoir.</p>
+<p>Lors de cette AG, Gilles Fourage co-président en fin de mandat, a annoncé qu'il ne renouvellait pas son mandat, pour des raisons d'éloignement géographique futur.</p>
+<p>Nous avons procédé à l'élection ou au renouvellement de 8 administrateurs.</p>
+<p>Le prochain conseil d'administration&nbsp; élira le bureau de l'USJA pour la saison 2016-2017.</p>
+<p>Retour en images :</p>
+]]></description>
+			<author>rozenn.fady@outlook.com (Rozenn Fady)</author>
+			<category>frontpage</category>
+			<pubDate>Sat, 03 Dec 2016 13:09:56 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
